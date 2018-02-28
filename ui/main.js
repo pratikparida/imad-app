27284735f87b1bfb6bc7e6770704d06998dceb15
pratikparida <36739906+pratikparida@users.noreply.github.com>
2018-02-28 22:39:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ element.innerHTML="New value";
 
 //to move the image on clicking on it
 var img = document.getElementById('madi');
-var marginLeft=0;
+var marginleft=0;
 function moveRight(){
     marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px' ;
+    img.style.marginLeft = marginleft + 'px' ;
 }
 img.onclick = function () {
       var interval = setInterval(moveRight, 50);
