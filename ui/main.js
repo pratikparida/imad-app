029db48311884a80b252017f66c57869bcfe95e7
pratikparida <36739906+pratikparida@users.noreply.github.com>
2018-02-28 22:10:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,9 @@
 console.log('Loaded!');
-
+//to change the content of the webpage
 var element = document.getElementById('main-text');
 element.innerHTML="New value";
+//to move the image on clicking on it
+var img = document.getElementbyId('madi');
+img.onclick=function (){
+    img.style.marginLeft='100px';
+}
