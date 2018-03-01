f49e0e38b891171a62ad5de1c39c535c0fa34386
pratikparida <36739906+pratikparida@users.noreply.github.com>
2018-03-01 13:18:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,10 +21,18 @@ button.onclick = function(){
   request.send(null);
 };
 //submit name
-var nameinput = document.getElementById('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request and send names
     //capture the list of names and render as a list
+    var names = ['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length,i++)
+    {
+        list=list+'<li>'+names[i]+'<li>';
+    }
+    var ul =document.getElementById('namelist');
+    ul.innerHTML=list;
 };
