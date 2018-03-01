6c15c179be9105f3ef0f74c5b08739d355230ed7
pratikparida <36739906+pratikparida@users.noreply.github.com>
2018-03-01 14:27:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,4 +20,18 @@ button.onclick = function(){
   request.open('GET','http://pratikkumarparida1112.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
 //submit name
+var nameInput= document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function(){
+    var anmes=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list =='<li>'+ names[i]+'</li>';
+    }
+    var ul =documet.getElementById('namelist');
+    ul.innerHTML = list;
+};
