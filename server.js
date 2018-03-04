c1d8b6929c7b4779e231b4983e8af02040486884
pratikparida <36739906+pratikparida@users.noreply.github.com>
2018-03-04 10:07:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user:'pratikkumarparida1112',
     database:'pratikkumarparida1112',
     host:'db.imad.hasura-app.io',
-    port:'5432',
+    port:'8080',
     password: process.env.DB_PASSWORD
 };
 
