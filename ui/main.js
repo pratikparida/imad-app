f12154e118dce882a2a1a8946344f2da9c71ff1b
pratikparida <36739906+pratikparida@users.noreply.github.com>
2018-03-06 15:00:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,11 +26,11 @@ var nameInput= document.getElementById('name');
 var name= nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
-    var anmes=['name1','name2','name3','name4'];
+    var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;i++)
     {
-        list =='<li>'+ names[i]+'</li>';
+        list = '<li>'+names[i]+'</li>';
     }
     var ul =documet.getElementById('namelist');
     ul.innerHTML = list;
